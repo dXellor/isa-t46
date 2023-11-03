1ce1ed5445c4798463af8198d5dde169a1b0b432
Nikola Simic <nikola7simic@gmail.com>
2023-11-03 18:17:04 +0100

    initial commit

---
+++ isa-back/src/test/java/com/isat46/isaback/IsaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isat46.isaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
