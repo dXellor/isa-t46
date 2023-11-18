bcc12e922097bce5f1eb9a4e0636cc82e06ebee7
Nikola Simic <nikola7simic@gmail.com>
2023-11-18 13:00:36 +0100

    chore: add dummy data to script

--- isa-back/src/main/resources/import.sql
+++ isa-back/src/main/resources/import.sql
@@ -1,3 +1,11 @@
-INSERT INTO isadb.role (name) VALUES ('ROLE_USER');
-INSERT INTO isadb.role (name) VALUES ('ROLE_SYSADMIN');
-INSERT INTO isadb.role (name) VALUES ('ROLE_COMPADMIN');
+INSERT INTO isadb."user" (city, company_information, country, email, enabled, first_name, last_name, last_password_reset_date, password, phone_number, profession) VALUES ('Novi Sad', 'Gaming INC', 'Serbia', 'slobodanmilosevic@gmail.com', true, 'Slobodan', 'Milosevic', '2023-10-01 21:58:58.508-07', '$2a$10$81IYoYJ.5JaYyImNOZMz0OSx9h1SH9BUz4HEURVx5dM7Us9yEmOde', '+381 123 312123', 'Gamer');
+INSERT INTO isadb."user" (city, company_information, country, email, enabled, first_name, last_name, last_password_reset_date, password, phone_number, profession) VALUES ('Novi Sad', 'Gaming INC', 'Serbia', 'petarpetrovic@gmail.com', true, 'Petar', 'Petrovic', '2023-10-01 21:58:58.508-07', '$2a$10$81IYoYJ.5JaYyImNOZMz0OSx9h1SH9BUz4HEURVx5dM7Us9yEmOde', '+381 123 312123', 'Gamer');
+INSERT INTO isadb."user" (city, company_information, country, email, enabled, first_name, last_name, last_password_reset_date, password, phone_number, profession) VALUES ('Novi Sad', 'Gaming INC', 'Serbia', 'jovanajovanovic@gmail.com', true, 'Jovana', 'Jovanovic', '2023-10-01 21:58:58.508-07', '$2a$10$81IYoYJ.5JaYyImNOZMz0OSx9h1SH9BUz4HEURVx5dM7Us9yEmOde', '+381 123 312123', 'Gamer');
+
+INSERT INTO isadb."role" (name) VALUES ('ROLE_USER');
+INSERT INTO isadb."role" (name) VALUES ('ROLE_SYSADMIN');
+INSERT INTO isadb."role" (name) VALUES ('ROLE_COMPADMIN');
+
+INSERT INTO isadb."user_role" (user_id, role_id) VALUES (1, 1);
+INSERT INTO isadb."user_role" (user_id, role_id) VALUES (2, 2);
+INSERT INTO isadb."user_role" (user_id, role_id) VALUES (3, 3);
