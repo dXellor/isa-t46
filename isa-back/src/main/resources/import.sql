bcfab12be0967b986cc1b719c0e34dae278abc2c
Nikola Simic <nikola7simic@gmail.com>
2023-12-15 12:47:20 +0100

    chore: add predefined appointments as reservations

--- isa-back/src/main/resources/import.sql
+++ isa-back/src/main/resources/import.sql
@@ -27,7 +27,6 @@ INSERT INTO isadb."equipment" (name, type, description, price) VALUES ('Bandage 
 INSERT INTO isadb."company_equipment" (company_id, equipment_id) VALUES (1, 1);
 INSERT INTO isadb."company_equipment" (company_id, equipment_id) VALUES (1, 2);
 
-
 INSERT INTO isadb."company_admin"(company_id, admin_id) VALUES (1, 3);
 INSERT INTO isadb."company_admin"(company_id, admin_id) VALUES (1, 2);
 
@@ -35,3 +34,7 @@ INSERT INTO isadb."company_equipment" (company_id, equipment_id) VALUES (2, 1);
 INSERT INTO isadb."company_equipment" (company_id, equipment_id) VALUES (2, 2);
 INSERT INTO isadb."company_equipment" (company_id, equipment_id) VALUES (2, 3);
 INSERT INTO isadb."company_equipment" (company_id, equipment_id) VALUES (2, 5);
+
+-- predefined appointments
+INSERT INTO isadb."reservation" (date_time, duration, status, admin_id) VALUES ('2023-12-15 11:29:20.376', 15, 0, 3)
+INSERT INTO isadb."reservation" (date_time, duration, status, admin_id) VALUES ('2023-12-10 12:00:00.0', 20, 0, 3)
