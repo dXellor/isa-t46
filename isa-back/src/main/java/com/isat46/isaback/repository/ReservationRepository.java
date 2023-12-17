9c966cdeaec8f2f798f8bd22099d77367a5f23db
Nikola Simić <nikola7simic@gmail.com>
2023-12-17 16:05:23 +0100

    fix: can not create appointment that intertwines with other reservations (#18)

--- isa-back/src/main/java/com/isat46/isaback/repository/ReservationRepository.java
+++ isa-back/src/main/java/com/isat46/isaback/repository/ReservationRepository.java
@@ -5,8 +5,15 @@ import com.isat46.isaback.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Integer> {
 
     Page<Reservation> findAll(Pageable pageable);
+
+    @Query("SELECT r FROM Reservation r WHERE r.companyAdmin.id = :companyAdminId AND (r.status = 0 OR r.status = 1)")
+    List<Reservation> findByCompanyAdminId(@Param("companyAdminId") int companyAdminId);
 }
