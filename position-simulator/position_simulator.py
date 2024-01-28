d9af36a570473512c9a5bc9060ba5d2970a9c01f
Nikola Simic <nikola7simic@gmail.com>
2024-01-28 15:56:24 +0100

    feat: position simulator logic, and predefined route

--- position-simulator/position_simulator.py
+++ position-simulator/position_simulator.py
@@ -2,24 +2,23 @@ import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 import threading
 import time
+import csv
 
 MQTT_HOST = 'localhost'
 MQTT_PORT = 1883
 MQTT_COMMAND_TOPIC = 'isat46/command'
 MQTT_LOCATION_TOPIC = 'isat46/location'
+CURRENT_SIMULATIONS = []
 
 def start_simulation(payload: bytes):
     try:
         command, reservation_id, start_latitude, start_longitude, end_latitude, end_longitude, delay = payload.decode('utf-8').split(';')
-        if command == 'BEGIN':
+        if command == 'BEGIN' and reservation_id not in CURRENT_SIMULATIONS:
+            CURRENT_SIMULATIONS.append(reservation_id)
             simulation_thread = threading.Thread(
                 target=simulation,
                 args=(
                     reservation_id,
-                    float(start_latitude),
-                    float(start_longitude),
-                    float(end_latitude),
-                    float(end_longitude),
                     int(delay),
                 )
             )
@@ -29,26 +28,23 @@ def start_simulation(payload: bytes):
     except ValueError:
         print("ERROR: could not parse command message")
 
-def simulation(reservation_id: str, start_latitude: float, start_longitude: float, end_latitude: float, end_longitude: float, delay: int):
-    while start_latitude == end_latitude or start_longitude == end_longitude:
-        try:
+def simulation(reservation_id: str, delay: int):
+    with open('sim-coords.csv') as file:
+        reader_obj = csv.DictReader(file)
 
-            publish.single(f'{MQTT_LOCATION_TOPIC}/{reservation_id}', 'test', qos=2)
-            print(f'{MQTT_LOCATION_TOPIC}/{reservation_id} TEST')
-        except:
-            print('failed to publish')
-        time.sleep(delay)
-
+        for row in reader_obj:
+            publish.single(f'{MQTT_LOCATION_TOPIC}/{reservation_id}', f'{row['lat;long']}', qos=2)
+            time.sleep(delay)
+    CURRENT_SIMULATIONS.remove(reservation_id)
 
 #MQTT
 def mqtt_on_connect(client, userdata, flags, rc):
     print(f'INFO: Connected to: {MQTT_HOST}:{MQTT_PORT}')
     client.subscribe(MQTT_COMMAND_TOPIC)
-    client.subscribe(f'{MQTT_LOCATION_TOPIC}/5')
 
 def mqtt_on_message(client, userdata, message):
     if(message.topic == MQTT_COMMAND_TOPIC):
-        start_simulation(client, message.payload)
+        start_simulation(message.payload)
     else:
         print("WARNING: Message received from non-command topic, check client subscribtions")
 
