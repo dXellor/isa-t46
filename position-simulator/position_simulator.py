1b6a26d6bf2e8ab8cc680014e99935b2af0b7de8
Nikola Simic <nikola7simic@gmail.com>
2024-01-28 11:58:39 +0100

    fix: publishing on location topic

--- position-simulator/position_simulator.py
+++ position-simulator/position_simulator.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
 import threading
 import time
 
@@ -7,14 +8,13 @@ MQTT_PORT = 1883
 MQTT_COMMAND_TOPIC = 'isat46/command'
 MQTT_LOCATION_TOPIC = 'isat46/location'
 
-def start_simulation(mqttClient: mqtt.Client, payload: bytes):
+def start_simulation(payload: bytes):
     try:
         command, reservation_id, start_latitude, start_longitude, end_latitude, end_longitude, delay = payload.decode('utf-8').split(';')
         if command == 'BEGIN':
             simulation_thread = threading.Thread(
                 target=simulation,
                 args=(
-                    mqttClient,
                     reservation_id,
                     float(start_latitude),
                     float(start_longitude),
@@ -29,10 +29,14 @@ def start_simulation(mqttClient: mqtt.Client, payload: bytes):
     except ValueError:
         print("ERROR: could not parse command message")
 
-def simulation(mqttclient: mqtt.Client, reservation_id: str, start_latitude: float, start_longitude: float, end_latitude: float, end_longitude: float, delay: int):
+def simulation(reservation_id: str, start_latitude: float, start_longitude: float, end_latitude: float, end_longitude: float, delay: int):
     while start_latitude == end_latitude or start_longitude == end_longitude:
-        print(f'{MQTT_LOCATION_TOPIC}/{reservation_id} TEST')
-        mqttclient.publish(f'{MQTT_LOCATION_TOPIC}/{reservation_id}', 'test')
+        try:
+
+            publish.single(f'{MQTT_LOCATION_TOPIC}/{reservation_id}', 'test', qos=2)
+            print(f'{MQTT_LOCATION_TOPIC}/{reservation_id} TEST')
+        except:
+            print('failed to publish')
         time.sleep(delay)
 
 
@@ -40,23 +44,22 @@ def simulation(mqttclient: mqtt.Client, reservation_id: str, start_latitude: flo
 def mqtt_on_connect(client, userdata, flags, rc):
     print(f'INFO: Connected to: {MQTT_HOST}:{MQTT_PORT}')
     client.subscribe(MQTT_COMMAND_TOPIC)
-    client.subscribe(f'{MQTT_LOCATION_TOPIC}/1')
+    client.subscribe(f'{MQTT_LOCATION_TOPIC}/5')
 
 def mqtt_on_message(client, userdata, message):
-    print(f'INFO: Message received from: {message.topic}: {message.payload.decode('utf-8')}')
-
     if(message.topic == MQTT_COMMAND_TOPIC):
         start_simulation(client, message.payload)
     else:
         print("WARNING: Message received from non-command topic, check client subscribtions")
 
 def main():
-    client = mqtt.Client()
+    client = mqtt.Client(client_id='POSSIM46')
     client.on_connect = mqtt_on_connect
     client.on_message = mqtt_on_message
 
     client.connect(MQTT_HOST, MQTT_PORT)
-    client.loop_forever() 
+    while True:
+        client.loop() 
 
 if __name__=='__main__':
     main()
